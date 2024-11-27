f0974e082a9f153be6d08235d2d33ba07a98438f
Lívia Silva <liviadfsilva@gmail.com>
2024-11-27 14:35:59 -0300

    Band Name Generator

--- Day01/main.py
+++ Day01/main.py
@@ -1,5 +1,5 @@
 # Band Name Generator
-print("Welcome to Band Name Generator!")
+print("Welcome to the Band Name Generator!")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's the name of your pet?\n")
 print("The name of your band could be: The " + city + " " + pet)
