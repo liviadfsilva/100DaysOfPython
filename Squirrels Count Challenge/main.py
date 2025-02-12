4e6a3b80d725e1850adbac1d57408c4a7420797d
Lívia Silva <liviadfsilva@gmail.com>
2025-02-12 18:27:02 -0300

    Day25 Challenge

---
+++ Squirrels Count Challenge/main.py
@@ -0,0 +1,15 @@
+import pandas
+
+data = pandas.read_csv("Squirrel_Data_2018.csv")
+
+gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels_count, cinnamon_squirrels_count, black_squirrels_count]
+}
+
+data_frame = pandas.DataFrame(data_dict)
+data_frame.to_csv("squirrel_count.csv")
