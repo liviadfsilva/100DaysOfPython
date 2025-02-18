e365ee398149bbd79801430b03d9f86c64c7ff2c
liviadfsilva <liviadfsilva@gmail.com>
2025-02-17 21:53:42 -0300

    added list comprehension

--- Day25/main.py
+++ Day25/main.py
@@ -3,7 +3,7 @@ import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
-image = "blank_states_img.gif"
+image = "Day25/blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
@@ -13,16 +13,13 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="Type a state name:").title()
 
-    data = pandas.read_csv("50_states.csv")
+    data = pandas.read_csv("Day25/50_states.csv")
     all_states = data.state.to_list()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
-        new_data.to_csv("states_to_learn.csv")
+        new_data.to_csv("Day25/states_to_learn.csv")
         break
     if answer_state in all_states:
         guessed_states.append(answer_state)
