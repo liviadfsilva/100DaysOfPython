d453051722538eeb191af8216de9e2775647b636
liviadfsilva <liviadfsilva@gmail.com>
2025-02-17 21:57:55 -0300

    states_to_learn.csv correction

--- Day25/main.py
+++ Day25/main.py
@@ -19,7 +19,7 @@ while len(guessed_states) < 50:
     if answer_state == "Exit":
         missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
-        new_data.to_csv("Day25/states_to_learn.csv")
+        new_data.to_csv("states_to_learn.csv")
         break
     if answer_state in all_states:
         guessed_states.append(answer_state)
