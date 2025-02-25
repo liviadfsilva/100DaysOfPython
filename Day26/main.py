8aff5e6414eb5ff914b030912b8d362207ea4cb1
liviadfsilva <liviadfsilva@gmail.com>
2025-02-25 17:38:34 -0300

    added try/except/else to file.

--- Day26/main.py
+++ Day26/main.py
@@ -4,6 +4,14 @@ data = "nato_phonetic_alphabet.csv"
 alphabet = pandas.read_csv(data)
 alphabet_dictionary = {row.letter:row.code for (index, row) in alphabet.iterrows()}
 
-user_word = input("Enter a word: ").upper()
-result = [alphabet_dictionary[letter] for letter in user_word if letter in alphabet_dictionary]
-print(result)
+no_word = True
+
+while no_word:
+    user_word = input("Enter a word: ").upper()
+    try:
+        result = [alphabet_dictionary[letter] for letter in user_word]
+        no_word = False
+    except KeyError:
+        print("Only one word is allowed. Try again.")
+    else:
+        print(result)
