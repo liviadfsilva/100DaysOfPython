67f02240babf2296a1f7b58a853d53b98b7d6cab
liviadfsilva <liviadfsilva@gmail.com>
2025-03-19 03:05:17 -0300

    canvas, buttons & label added to ui.py

--- Day34/ui.py
+++ Day34/ui.py
@@ -7,7 +7,23 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
-        self.window.config(bg=THEME_COLOR)
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.canvas.grid(row=1, columnspan=2, column=0, pady=20, padx=20)
+        self.canvas_question = self.canvas.create_text(150, 125, text="question here", font=("Arial", 20, "italic"),
+                                                       fill="black")
+
+        self.true_button_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=self.true_button_img)
+        self.true_button.grid(row=2, column=0)
+
+        self.false_button_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=self.false_button_img)
+        self.false_button.grid(row=2, column=1)
 
         self.window.mainloop()
 
