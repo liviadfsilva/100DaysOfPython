412774b647757ff63a13ad3a98efcbc46140e8a9
liviadfsilva <liviadfsilva@gmail.com>
2025-03-19 03:11:10 -0300

    set buttons' highlight thickness to zero

--- Day34/ui.py
+++ Day34/ui.py
@@ -13,16 +13,16 @@ class QuizInterface:
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
-        self.canvas.grid(row=1, columnspan=2, column=0, pady=20, padx=20)
+        self.canvas.grid(row=1, columnspan=2, column=0, pady=50)
         self.canvas_question = self.canvas.create_text(150, 125, text="question here", font=("Arial", 20, "italic"),
                                                        fill="black")
 
         self.true_button_img = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=self.true_button_img)
+        self.true_button = Button(image=self.true_button_img, highlightthickness=0)
         self.true_button.grid(row=2, column=0)
 
         self.false_button_img = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=self.false_button_img)
+        self.false_button = Button(image=self.false_button_img, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
         self.window.mainloop()
