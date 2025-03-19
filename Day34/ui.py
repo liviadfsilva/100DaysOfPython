364da176ab9abfffc00210b4dd2d6d8a25c3ff1a
liviadfsilva <liviadfsilva@gmail.com>
2025-03-19 02:40:14 -0300

    QuizzInterface class created

--- Day34/ui.py
+++ Day34/ui.py
@@ -1,2 +1,13 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR)
+
+        self.window.mainloop()
 
