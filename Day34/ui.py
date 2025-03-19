21cbccc5f38ff165190b9069a0d5bad034b07eca
liviadfsilva <liviadfsilva@gmail.com>
2025-03-19 14:16:45 -0300

    get_next_question method so the question will be on ui

--- Day34/ui.py
+++ Day34/ui.py
@@ -1,10 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -14,8 +17,8 @@ class QuizInterface:
 
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.canvas.grid(row=1, columnspan=2, column=0, pady=50)
-        self.canvas_question = self.canvas.create_text(150, 125, text="question here", font=("Arial", 20, "italic"),
-                                                       fill="black")
+        self.canvas_question = self.canvas.create_text(150, 125, width=280, text="question here",
+                                                       font=("Arial", 20, "italic"), fill="black")
 
         self.true_button_img = PhotoImage(file="images/true.png")
         self.true_button = Button(image=self.true_button_img, highlightthickness=0)
@@ -25,5 +28,11 @@ class QuizInterface:
         self.false_button = Button(image=self.false_button_img, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        question_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.canvas_question, text=question_text)
 
