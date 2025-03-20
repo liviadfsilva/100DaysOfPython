c31aefdeb038b8dd0a3d89df681b9a5eb085fadc
liviadfsilva <liviadfsilva@gmail.com>
2025-03-20 14:52:47 -0300

    give_feedback method created in ui.py

--- Day34/ui.py
+++ Day34/ui.py
@@ -33,12 +33,20 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
+        self.canvas.config(bg="white")
         question_text = self.quiz.next_question()
         self.canvas.itemconfig(self.canvas_question, text=question_text)
 
     def true_click(self):
-        self.quiz.check_answer("True")
+        self.give_feedback(self.quiz.check_answer("True"))
 
     def false_click(self):
-        self.quiz.check_answer("False")
+        self.give_feedback(self.quiz.check_answer("False"))
 
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+            self.window.after(1000, self.get_next_question)
+        else:
+            self.canvas.config(bg="red")
+            self.window.after(1000, self.get_next_question)
