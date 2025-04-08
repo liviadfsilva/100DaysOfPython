37c7ab65af7df987611ae59b49d24b15dca434df
liviadfsilva <liviadfsilva@gmail.com>
2025-04-07 22:06:07 -0300

    web scraping project upload

--- Beautiful Soup/main.py
+++ Beautiful Soup/main.py
@@ -1,7 +1,40 @@
 from bs4 import BeautifulSoup
-import lxml
+import requests
 
-with open("/Users/liviasilva/Documents/Projects/100DaysOfPython/Beautiful Soup/website.html") as web_page:
-    content = web_page.read()
+response = requests.get(url="https://news.ycombinator.com/")
+
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+
+article_tag = soup.find_all(name="span", class_="titleline")
+
+article_titles = []
+article_links = []
+article_upvotes = []
+
+for i in article_tag:
+    a_tag = i.find("a")      
     
-soup = BeautifulSoup(content, "lxml")
+    if a_tag:
+        upvotes = i.find_next(name="span", class_="score")
+        if upvotes:
+            all_votes = int(upvotes.getText().split()[0])
+            article_upvotes.append(all_votes)
+        
+        href = a_tag.get("href")
+        article_links.append(href)
+        
+        article_text = a_tag.getText()
+        article_titles.append(article_text)
+        
+# print(article_titles)
+# print(article_links)
+# print(article_upvotes)
+
+largest_number = max(article_upvotes)
+max_index = article_upvotes.index(largest_number)
+
+print(article_titles[max_index])
+print(article_links[max_index])
+print(largest_number)
