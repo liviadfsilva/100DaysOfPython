50f6a69df56338406e320eb216660a65b395aea0
liviadfsilva <liviadfsilva@gmail.com>
2025-04-08 19:27:31 -0300

    got all titles

--- Day45/main.py
+++ Day45/main.py
@@ -6,4 +6,19 @@ movies_web_page = response.text
 
 soup = BeautifulSoup(movies_web_page, "html.parser")
 
-print(soup.title)
+articles = soup.find("div", class_="article_article-content__3auQJ false")
+titles = articles.find_all("strong")
+
+titles_only = []
+
+for i in titles:
+    result = i.getText()
+    titles_only.append(result)
+            
+# print(titles_only)
+
+for i in titles_only:
+    if i == "Director:" or i == "Starring:":
+        titles_only.remove(i)
+        
+print(titles_only)
