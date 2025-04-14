5226ae18e473874cb99d9e5fb6875823c252264f
liviadfsilva <liviadfsilva@gmail.com>
2025-04-14 20:46:39 -0300

    added copyright to footer with dynamic year

---
+++ Day57/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    current_year = datetime.now().strftime("%Y")
+    return render_template("index.html", year=current_year)
+
+if __name__ == "__main__":
+    app.run(debug=True)
