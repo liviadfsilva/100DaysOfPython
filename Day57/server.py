22ee93a86dc50cdde6c622fe013430b706e339f6
liviadfsilva <liviadfsilva@gmail.com>
2025-04-15 15:01:02 -0300

    added gender api and guess.html

--- Day57/server.py
+++ Day57/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -7,6 +8,15 @@ app = Flask(__name__)
 def home():
     current_year = datetime.now().strftime("%Y")
     return render_template("index.html", year=current_year)
+
+@app.route("/guess/<name>")
+def guess(name):
+    gender_url = f"https://api.genderize.io?name={name}"
+    gender_response = requests.get(gender_url)
+    gender_data = gender_response.json()
+    gender = gender_data["gender"]
+    
+    return render_template("guess.html", person_name=name, person_gender=gender)
 
 if __name__ == "__main__":
     app.run(debug=True)
