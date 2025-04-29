fd262fcf56a288af4ab1174fba3270ab3c7362fe
liviadfsilva <liviadfsilva@gmail.com>
2025-04-29 16:02:40 -0300

    made contact and about page work

--- Day59/main.py
+++ Day59/main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact_page():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
