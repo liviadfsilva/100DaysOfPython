c859cb314378f91efa2b4b2af7e7cc6aa18b4353
liviadfsilva <liviadfsilva@gmail.com>
2025-05-01 14:59:52 -0300

    made post request to request name and password

--- Day60/main.py
+++ Day60/main.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def receive_data():
+    error = None
+    if request.method == 'POST':
+        name = request.form["fname"]
+        password = request.form['password']
+        return f"<h1>Name: {name}, Password:{password}<h1>"
+    else:
+        error = 'Invalid username/password'
+        return error
 
 if __name__ == "__main__":
     app.run(debug=True)
