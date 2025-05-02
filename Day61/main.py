73b3de3956041f4800f8d5631b3a313d5d24b033
liviadfsilva <liviadfsilva@gmail.com>
2025-05-02 15:06:35 -0300

    created login form with flask wtf and added env and env.example

--- Day61/main.py
+++ Day61/main.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
+    
+app.secret_key = os.getenv("APP_SECRET_KEY")
 
 @app.route("/")
 def home():
@@ -8,7 +22,8 @@ def home():
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    login_form = LoginForm()
+    return render_template("login.html", form=login_form)
 
 if __name__ == "__main__":
     app.run(debug=True)
