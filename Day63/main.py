0da0e80ed3d8fec7adf964880e0422c8a41ce880
liviadfsilva <liviadfsilva@gmail.com>
2025-05-14 18:21:53 -0300

    created a database to store books and its reviews

--- Day63/main.py
+++ Day63/main.py
@@ -1,22 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = Flask(__name__)
 
-all_books = []
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/liviasilva/Documents/Projects/100DaysOfPython/Day63/book-reviews-collection.db"
+db.init_app(app)
+
+class Books(db.Model):
+    __tablename__ = "Reviews"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(unique=True)
+    author: Mapped[str] = mapped_column(nullable=False)
+    rating: Mapped[str] = mapped_column(nullable=False)
+    
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
-    return render_template("index.html", books=all_books)
+    with app.app_context():
+        result = db.session.execute(db.select(Books).order_by(Books.title)).scalars().all()
+    return render_template("index.html", books=result)
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(new_book)
+        with app.app_context():
+            new_book = Books(title=request.form["title"], author=request.form["author"], rating=request.form["rating"])
+            db.session.add(new_book)
+            db.session.commit()
         return redirect(url_for('home'))
     return render_template("add.html")
 
