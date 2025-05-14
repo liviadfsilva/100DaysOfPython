781511ae9194606c571834a364964a53956a1c89
liviadfsilva <liviadfsilva@gmail.com>
2025-05-14 18:54:38 -0300

    added /delete to delete books

--- Day63/main.py
+++ Day63/main.py
@@ -49,6 +49,14 @@ def edit_rating():
     book_selected = db.get_or_404(Books, book_id)
     return render_template("edit.html", book=book_selected)
 
+@app.route('/delete')
+def delete_book():
+    book_id = request.args.get('id')
+    book_to_delete = db.get_or_404(Books, book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
 
