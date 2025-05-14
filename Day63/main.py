87f300385dc5da771d374cf65447cf87a3362b03
liviadfsilva <liviadfsilva@gmail.com>
2025-05-14 18:46:17 -0300

    added edit.html to edit book rating

--- Day63/main.py
+++ Day63/main.py
@@ -37,6 +37,18 @@ def add():
         return redirect(url_for('home'))
     return render_template("add.html")
 
+@app.route('/edit', methods=["GET", "POST"])
+def edit_rating():
+    if request.method == "POST":
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Books, book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Books, book_id)
+    return render_template("edit.html", book=book_selected)
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
 
