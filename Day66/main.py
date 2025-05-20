bb13c123ac43c7ac2e45ed28432746a656ad31a8
liviadfsilva <liviadfsilva@gmail.com>
2025-05-20 16:00:27 -0300

    got a random cafe and returned in json

--- Day66/main.py
+++ Day66/main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
+import random
 
 app = Flask(__name__)
 
@@ -12,7 +13,6 @@ class Base(DeclarativeBase):
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/liviasilva/Documents/Projects/100DaysOfPython/Day66/instance/cafes.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
-
 
 # Cafe TABLE Configuration
 class Cafe(db.Model):
@@ -27,7 +27,12 @@ class Cafe(db.Model):
     has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
-
+    
+    def to_dict(self):
+        dictionary = {}
+        for column in self.__table__.columns:
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
 
 with app.app_context():
     db.create_all()
@@ -37,10 +42,11 @@ def home():
     return render_template("index.html")
 
 @app.route("/random")
-def random():
-    pass
-
-
+def get_random_cafe():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    random_cafe = random.choice(all_cafes)
+    return jsonify(cafe=random_cafe.to_dict())
 
 # HTTP GET - Read Record
 
@@ -49,7 +55,6 @@ def random():
 # HTTP PUT/PATCH - Update Record
 
 # HTTP DELETE - Delete Record
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
