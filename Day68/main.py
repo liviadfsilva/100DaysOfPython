543f41f5cfa10c0cb94a1fbb97b32ab5ed8b3906
liviadfsilva <liviadfsilva@gmail.com>
2025-05-29 17:59:37 -0300

    created .env.example and made login work

--- Day68/main.py
+++ Day68/main.py
@@ -3,13 +3,25 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
-# from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+
+# Login Manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# User Callback
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
 
 # CREATE DATABASE
-
 class Base(DeclarativeBase):
     pass
 
@@ -19,13 +31,11 @@ db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 # CREATE TABLE IN DB
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
     name: Mapped[str] = mapped_column(String(1000))
-
 
 with app.app_context():
     db.create_all()
@@ -37,29 +47,50 @@ def home():
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        hash_and_salted_password = generate_password_hash(
+            request.form.get('password'),
+            method="pbkdf2:sha256",
+            salt_length=8
+        )
+        
         new_user = User(
             email=request.form.get('email'),
-            password=request.form.get('password'),
+            password=hash_and_salted_password,
             name=request.form.get('name')
         )
         db.session.add(new_user)
         db.session.commit()
-        return render_template('secrets.html', name=request.form.get('name'))
+        return render_template('login.html')
     return render_template("register.html")
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+        
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('secrets'))
     return render_template("login.html")
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    print(current_user.name)
+    return render_template("secrets.html", name=current_user.name)
 
 @app.route('/logout')
+@login_required
 def logout():
+    logout_user()
     pass
 
 @app.route('/download')
+@login_required
 def download():
     return send_from_directory("static", path="files/cheat_sheet.pdf", as_attachment=True)
 
