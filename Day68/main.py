109799a04ad659aa0229243ea855686e27ded3da
liviadfsilva <liviadfsilva@gmail.com>
2025-05-30 16:04:02 -0300

    final touches: logged_in=current_user.is_authenticated

--- Day68/main.py
+++ Day68/main.py
@@ -42,11 +42,19 @@ with app.app_context():
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        email = request.form.get('email')
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+        
+        if user:
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+        
         hash_and_salted_password = generate_password_hash(
             request.form.get('password'),
             method="pbkdf2:sha256",
@@ -60,7 +68,7 @@ def register():
         )
         db.session.add(new_user)
         db.session.commit()
-        return render_template('login.html')
+        return redirect(url_for('secrets'))
     return render_template("register.html")
 
 @app.route('/login', methods=["GET", "POST"])
@@ -72,22 +80,28 @@ def login():
         result = db.session.execute(db.select(User).where(User.email == email))
         user = result.scalar()
         
-        if check_password_hash(user.password, password):
+        if not user:
+            flash("The email doesn't exist, please try again.")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash("Password incorrect, please try again.")
+            return redirect(url_for('login'))
+        else:
             login_user(user)
             return redirect(url_for('secrets'))
-    return render_template("login.html")
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 @app.route('/secrets')
 @login_required
 def secrets():
     print(current_user.name)
-    return render_template("secrets.html", name=current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    pass
+    return redirect(url_for('home'))
 
 @app.route('/download')
 @login_required
